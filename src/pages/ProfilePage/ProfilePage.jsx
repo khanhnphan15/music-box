b4280a93a65a914fc4cfb425c4576572c478bfa2
JimHaff <james.haff@generalassemb.ly>
2023-07-27 11:05:10 -0500

    Added profilepage

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom'
 import { Grid } from "semantic-ui-react";
 import ProfileBio from "../../components/ProfileBio/ProfileBio";
 import ProfilePostDisplay from "../../components/ProfilePostDisplay/ProfilePostDisplay";
 import PageHeader from "../../components/Header/Header";
 
+import userService from "../../utils/userService";
+
 export default function ProfilePage() {
+
+
+ // grabbing the param from this route
+ //  <Route path="/:username" element={<ProfilePage />} />
+ const { username } = useParams()
+ console.log(username)
+ 
+
   return (
     <Grid>
       <Grid.Row>
